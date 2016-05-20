b985744676db8299ed558f0efdeecd45bf58e82c
Takashi OGURA <takashi-ogura@cookpad.com>
2016-05-20 12:46:18 +0900

    Start learning java

---
+++ java/Practice1_2.java
@@ -0,0 +1,8 @@
+public class Practice1_2 {
+  public static void main ( String[ ] args ) {
+    int a = 3;
+    int b = 5;
+    System.out.println( "縦幅 " + a + " 横幅 " + b + " の長方形の面積は、 " + a * b );
+  }
+}
+
